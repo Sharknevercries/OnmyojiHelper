58951b731ce958de40c89d6b3883e1c6457f2770
sharknevercries <sharknevercries@gmail.com>
2017-06-10 15:35:10 +0800

    Add data info.

---
+++ OnmyojiHelper/Models/Bounty.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnmyojiHelper.Models
+{
+    [Table("Bounty")]
+    public class Bounty
+    {
+        public int Id { get; set; }
+
+        public int ShikigamiId { get; set; }
+        public Shikigami Shikigami { get; set; }
+
+        public ICollection<Relations.BountyClue> BountyClues { get; set; }
+    }
+}
---
+++ OnmyojiHelper/Models/Relations/ShikigamiBattle.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnmyojiHelper.Models.Relations
+{
+    [Table("ShikigamiBattle")]
+    public class ShikigamiBattle
+    {
+        public int ShikigamiId { get; set; }
+        public Shikigami Shikigami { get; set; }
+
+        public int BattleId { get; set; }
+        public Battle Battle { get; set; }
+
+        public int Count { get; set; }
+    }
+}

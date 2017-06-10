917211d6f900283a93724fe9b3ec1281b5e2ef79
sharknevercries <sharknevercries@gmail.com>
2017-06-10 14:43:59 +0800

    Add project files.

---
+++ OnmyojiHelper/Views/DetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using OnmyojiHelper.ViewModels;
+using Windows.UI.Xaml.Navigation;
+using Windows.UI.Xaml.Controls;
+
+namespace OnmyojiHelper.Views
+{
+    public sealed partial class DetailPage : Page
+    {
+        public DetailPage()
+        {
+            InitializeComponent();
+            NavigationCacheMode = NavigationCacheMode.Disabled;
+        }
+    }
+}

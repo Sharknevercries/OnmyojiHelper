0e155a4afb09bd85f5bbf86b307097ebfae20982
sharknevercries <sharknevercries@gmail.com>
2017-06-10 15:42:16 +0800

    InitMigration.

---
+++ OnmyojiHelper/Migrations/20170610074116_InitMigration.cs
@@ -0,0 +1,185 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace OnmyojiHelper.Migrations
+{
+    public partial class InitMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Clue",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Keyword = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Clue", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Shikigami",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(nullable: true),
+                    Rarity = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Shikigami", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Stage",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Category = table.Column<int>(nullable: false),
+                    Title = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Stage", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Bounty",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    ShikigamiId = table.Column<int>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Bounty", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Bounty_Shikigami_ShikigamiId",
+                        column: x => x.ShikigamiId,
+                        principalTable: "Shikigami",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Battle",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    StageId = table.Column<int>(nullable: true),
+                    Title = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Battle", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Battle_Stage_StageId",
+                        column: x => x.StageId,
+                        principalTable: "Stage",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "BountyClue",
+                columns: table => new
+                {
+                    BountyId = table.Column<int>(nullable: false),
+                    ClueId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_BountyClue", x => new { x.BountyId, x.ClueId });
+                    table.ForeignKey(
+                        name: "FK_BountyClue_Bounty_BountyId",
+                        column: x => x.BountyId,
+                        principalTable: "Bounty",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_BountyClue_Clue_ClueId",
+                        column: x => x.ClueId,
+                        principalTable: "Clue",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ShikigamiBattle",
+                columns: table => new
+                {
+                    ShikigamiId = table.Column<int>(nullable: false),
+                    BattleId = table.Column<int>(nullable: false),
+                    Count = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ShikigamiBattle", x => new { x.ShikigamiId, x.BattleId });
+                    table.ForeignKey(
+                        name: "FK_ShikigamiBattle_Battle_BattleId",
+                        column: x => x.BattleId,
+                        principalTable: "Battle",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_ShikigamiBattle_Shikigami_ShikigamiId",
+                        column: x => x.ShikigamiId,
+                        principalTable: "Shikigami",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Battle_StageId",
+                table: "Battle",
+                column: "StageId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Bounty_ShikigamiId",
+                table: "Bounty",
+                column: "ShikigamiId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_BountyClue_ClueId",
+                table: "BountyClue",
+                column: "ClueId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ShikigamiBattle_BattleId",
+                table: "ShikigamiBattle",
+                column: "BattleId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "BountyClue");
+
+            migrationBuilder.DropTable(
+                name: "ShikigamiBattle");
+
+            migrationBuilder.DropTable(
+                name: "Bounty");
+
+            migrationBuilder.DropTable(
+                name: "Clue");
+
+            migrationBuilder.DropTable(
+                name: "Battle");
+
+            migrationBuilder.DropTable(
+                name: "Shikigami");
+
+            migrationBuilder.DropTable(
+                name: "Stage");
+        }
+    }
+}
---
+++ OnmyojiHelper/Migrations/OnmyojiContextModelSnapshot.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using OnmyojiHelper;
+using OnmyojiHelper.Models.Enums;
+
+namespace OnmyojiHelper.Migrations
+{
+    [DbContext(typeof(OnmyojiContext))]
+    partial class OnmyojiContextModelSnapshot : ModelSnapshot
+    {
+        protected override void BuildModel(ModelBuilder modelBuilder)
+        {
+            modelBuilder
+                .HasAnnotation("ProductVersion", "1.1.2");
+
+            modelBuilder.Entity("OnmyojiHelper.Models.Battle", b =>
+                {
+                    b.Property<int>("Id")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<int?>("StageId");
+
+                    b.Property<string>("Title");
+
+                    b.HasKey("Id");
+
+                    b.HasIndex("StageId");
+
+                    b.ToTable("Battle");
+                });
+
+            modelBuilder.Entity("OnmyojiHelper.Models.Bounty", b =>
+                {
+                    b.Property<int>("Id")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<int?>("ShikigamiId");
+
+                    b.HasKey("Id");
+
+                    b.HasIndex("ShikigamiId");
+
+                    b.ToTable("Bounty");
+                });
+
+            modelBuilder.Entity("OnmyojiHelper.Models.Clue", b =>
+                {
+                    b.Property<int>("Id")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<string>("Keyword");
+
+                    b.HasKey("Id");
+
+                    b.ToTable("Clue");
+                });
+
+            modelBuilder.Entity("OnmyojiHelper.Models.Relations.BountyClue", b =>
+                {
+                    b.Property<int>("BountyId");
+
+                    b.Property<int>("ClueId");
+
+                    b.HasKey("BountyId", "ClueId");
+
+                    b.HasIndex("ClueId");
+
+                    b.ToTable("BountyClue");
+                });
+
+            modelBuilder.Entity("OnmyojiHelper.Models.Relations.ShikigamiBattle", b =>
+                {
+                    b.Property<int>("ShikigamiId");
+
+                    b.Property<int>("BattleId");
+
+                    b.Property<int>("Count");
+
+                    b.HasKey("ShikigamiId", "BattleId");
+
+                    b.HasIndex("BattleId");
+
+                    b.ToTable("ShikigamiBattle");
+                });
+
+            modelBuilder.Entity("OnmyojiHelper.Models.Shikigami", b =>
+                {
+                    b.Property<int>("Id")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<string>("Name");
+
+                    b.Property<int>("Rarity");
+
+                    b.HasKey("Id");
+
+                    b.ToTable("Shikigami");
+                });
+
+            modelBuilder.Entity("OnmyojiHelper.Models.Stage", b =>
+                {
+                    b.Property<int>("Id")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<int>("Category");
+
+                    b.Property<string>("Title");
+
+                    b.HasKey("Id");
+
+                    b.ToTable("Stage");
+                });
+
+            modelBuilder.Entity("OnmyojiHelper.Models.Battle", b =>
+                {
+                    b.HasOne("OnmyojiHelper.Models.Stage")
+                        .WithMany("Battles")
+                        .HasForeignKey("StageId");
+                });
+
+            modelBuilder.Entity("OnmyojiHelper.Models.Bounty", b =>
+                {
+                    b.HasOne("OnmyojiHelper.Models.Shikigami", "Shikigami")
+                        .WithMany()
+                        .HasForeignKey("ShikigamiId");
+                });
+
+            modelBuilder.Entity("OnmyojiHelper.Models.Relations.BountyClue", b =>
+                {
+                    b.HasOne("OnmyojiHelper.Models.Bounty", "Bounty")
+                        .WithMany("BountyClues")
+                        .HasForeignKey("BountyId")
+                        .OnDelete(DeleteBehavior.Cascade);
+
+                    b.HasOne("OnmyojiHelper.Models.Clue", "Clue")
+                        .WithMany("BountyClues")
+                        .HasForeignKey("ClueId")
+                        .OnDelete(DeleteBehavior.Cascade);
+                });
+
+            modelBuilder.Entity("OnmyojiHelper.Models.Relations.ShikigamiBattle", b =>
+                {
+                    b.HasOne("OnmyojiHelper.Models.Battle", "Battle")
+                        .WithMany("ShikigamiBattles")
+                        .HasForeignKey("BattleId")
+                        .OnDelete(DeleteBehavior.Cascade);
+
+                    b.HasOne("OnmyojiHelper.Models.Shikigami", "Shikigami")
+                        .WithMany("ShikigamiBattles")
+                        .HasForeignKey("ShikigamiId")
+                        .OnDelete(DeleteBehavior.Cascade);
+                });
+        }
+    }
+}
